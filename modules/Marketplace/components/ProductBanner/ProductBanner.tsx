1ffcf0bf048ccd908a70753c5f65b1fa56316c2d
Artyom Narshinov <narshinovartyom@mail.ru>
2024-04-29 21:57:47 +0400

    preview slider

--- modules/Marketplace/components/ProductBanner/ProductBanner.tsx
+++ modules/Marketplace/components/ProductBanner/ProductBanner.tsx
@@ -8,6 +8,7 @@ import React, {FC} from 'react';
 import s from './ProductBanner.module.scss';
 
 import banner from 'public/image/marketplace-banner.png';
+import {Carousel} from 'antd';
 
 interface ProductBannerProps {
   title: string;
@@ -20,7 +21,13 @@ export const ProductBanner: FC<ProductBannerProps> = ({title, price}) => {
       <h2 className='font-medium text-xl mb-10'>Информация о товаре</h2>
 
       <div className={s.wrapper}>
-        <PreloaderImage src={banner} objectFit='cover' alt='' width={500} height={500} className={s.image} />
+        <div className={s.carousel}>
+          <Carousel autoplay>
+            <PreloaderImage src={banner} className={s.image} objectFit='cover' alt='' width={200} height={200} />
+            <PreloaderImage src={banner} className={s.image} objectFit='cover' alt='' width={200} height={200} />
+            <PreloaderImage src={banner} className={s.image} objectFit='cover' alt='' width={200} height={200} />
+          </Carousel>
+        </div>
         <div className={s.content}>
           <h1 className={s.title}>{title}</h1>
           <p className={s.price}>{formatProductPrice(price)}</p>
