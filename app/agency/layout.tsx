1666bc0b2fe930d1037e3f3dc875adc9fd92cfeb
Artyom Narshinov <narshinovartyom@mail.ru>
2024-05-08 19:23:39 +0400

    final

--- app/agency/layout.tsx
+++ app/agency/layout.tsx
@@ -1,38 +1,8 @@
 import {Footer} from '@/components/Footer/Footer';
 import {Header} from '@/components/Header/Header';
 import '@/src/styles/global.scss';
-import {Metadata} from 'next';
 
 import React from 'react';
-
-export const metadata: Metadata = {
-  title: {
-    default: 'Webi Agency',
-    template: `%s | Webi`
-  },
-  description: 'Купить веб сайт или веб приложение',
-  keywords: [
-    'веб приложение',
-    'создание сайтов',
-    'разработка сайтов',
-    'landing',
-    'e commerce',
-    'самый дешевый интернет магазин'
-  ],
-  robots: {
-    index: true,
-    follow: true
-  },
-  icons: {
-    icon: '/favicon.ico'
-  },
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false
-  }
-};
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
