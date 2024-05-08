df45a4493548398fa5769fabb55d8fb5c2281379
Artyom Narshinov <narshinovartyom@mail.ru>
2024-05-08 19:28:55 +0400

    ym

--- app/layout.tsx
+++ app/layout.tsx
@@ -104,6 +104,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         </Script>
         <noscript>
           <div>
+            {/* @ts-ignore */}
             <img src='https://mc.yandex.ru/watch/94315700' style='position:absolute; left:-9999px;' alt='' />
           </div>
         </noscript>
